b6bdd3ceac3fdcb04e948105457f632f748bb65c
Yanikoo <nikov1768@gmail.com>
2024-11-26 23:29:08 +0300

    make lake

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, provide, reactive, ref, watch, } from 'vue'
+import { onMounted, provide, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 
 import Header from './components/Header.vue'
@@ -23,12 +23,12 @@ const onChangeSearchInput = (event) => {
 
 const fetchFavorites = async () => {
   try {
-    const {data: favorites } = await axios.get(`https://f32b821cc4008a80.mokky.dev/favorites`)
+    const { data: favorites } = await axios.get(`https://f32b821cc4008a80.mokky.dev/favorites`)
 
-    items.value = items.value.map(item => {
-      const favorite = favorites.find(favorite =>favorite.parentId === item.id);
+    items.value = items.value.map((item) => {
+      const favorite = favorites.find((favorite) => favorite.parentId === item.id)
 
-      if(!favorite) {
+      if (!favorite) {
         return item
       }
 
@@ -36,24 +36,21 @@ const fetchFavorites = async () => {
         ...item,
         isFavorite: true,
         favoriteId: favorite.id
-      };
+      }
     })
-
   } catch (err) {
     console.log(err)
   }
 }
 
 const addToFavorite = async (item) => {
-  item.isFavorite = true
-  
+  item.isFavorite = !item.isFavorite
+
   console.log(item)
-  
 }
 
 const fetchItem = async () => {
   try {
-    
     const params = {
       sortBy: filters.sortBy
     }
@@ -62,7 +59,7 @@ const fetchItem = async () => {
       params.title = `*${filters.searchQuery}*`
     }
 
-    const {data} = await axios.get(`https://f32b821cc4008a80.mokky.dev/sneakers`, {
+    const { data } = await axios.get(`https://f32b821cc4008a80.mokky.dev/sneakers`, {
       params
     })
 
@@ -77,52 +74,48 @@ const fetchItem = async () => {
 }
 
 onMounted(async () => {
-   await fetchItem();
-   await fetchFavorites();
-  
+  await fetchItem()
+  await fetchFavorites()
 })
-watch(filters,fetchItem)
+watch(filters, fetchItem)
 
 provide('addToFavorite', addToFavorite)
 </script>
 
 <template>
-  
   <div>
     <!-- <Drawer /> -->
-  
-    <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14"> 
+
+    <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
       <Header />
-  
+
       <div class="px-10">
         <div class="flex justify-between items-center">
           <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-        
-          <div class="flex gap-4"> 
-             <select @change="onChangeSelect" class="py-2 px-3 border rounded-mb outline-none">
+
+          <div class="flex gap-4">
+            <select @change="onChangeSelect" class="py-2 px-3 border rounded-mb outline-none">
               <option value="name">По названию</option>
               <option value="price">По цене ( дешевые)</option>
               <option value="-price">По цене ( дорогие)</option>
             </select>
-              
-           
-    
+
             <div class="relative">
               <img src="/search.svg" class="absolute left-3 top-3" />
-              <input 
+              <input
                 @input="onChangeSearchInput"
-                class="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400 " 
+                class="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
                 type="text"
                 placeholder="Поиск..."
               />
             </div>
           </div>
-        </div>  
-        <div class="mt-10"> 
-          <CardList  :items="items"/>
+        </div>
+
+        <div class="mt-10">
+          <CardList :items="items" @addToFavorite="addToFavorite" />
         </div>
       </div>
     </div>
   </div>
 </template>
-
--- vue-sneakers/src/components/CardList.vue
+++ vue-sneakers/src/components/CardList.vue
@@ -1,40 +1,30 @@
 <script setup>
-import { inject } from 'vue'
 import Card from './Card.vue'
- 
 
 defineProps({
-    items: Array
+  items: Array
 })
-
-const addToFavorite = inject('addToFavorite')
-
-const onClickFavorite = () => {
-  const obj = {
-    ...props,
-    parentId: props.id 
-  }
-
-  addToFavorite(obj)
-}
+const emit = defineEmits(['addToFavorite'])
 
 const onClickAdd = () => {
-    alert('Добавлено в корзину')
+  alert('Добавлено в корзину')
 }
+
+const addToFavorite = inject('addToFavorite')
 </script>
 
 <template>
-    <div class="grid grid-cols-4 gap-5 mt-8">
-        <Card 
-            v-for="item in items"
-            :key="item.id"
-            :id="item.id"
-            :title="item.title"
-            :image-url="item.imageUrl"
-            :price="item.price"
-            :onClickAdd="onClickAdd"
-            :isFavorite="item.isFavorite"
-        />
-    </div>
-    
+  <div class="grid grid-cols-4 gap-5 mt-8">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :image-url="item.imageUrl"
+      :price="item.price"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="() => addToFavorite(item)"
+      :isFavorite="item.isFavorite"
+    />
+  </div>
 </template>
